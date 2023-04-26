4a5dbc55373faf2386f5d3ac980ba0d01ae3b901
ASUS <ms0565119@hotmail.com>
2023-04-26 10:08:03 +0800

    first

---
+++ src/main/java/com/example/course_select_system/CourseSelectSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_select_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSelectSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CourseSelectSystemApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/course_select_system/CourseSelectSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_select_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSelectSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
